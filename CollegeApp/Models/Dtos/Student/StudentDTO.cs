eeb4ebad6ed11ebbc587c6856e09152d296a50d2
yunusyunu94 <125892629+yunusyunu94@users.noreply.github.com>
2024-10-07 22:19:25 +0300

    Duzeltme

---
+++ CollegeApp/Models/Dtos/Student/StudentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CollegeApp.Models.Dtos.Student
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+        public string SutudentName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
