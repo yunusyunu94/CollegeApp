46612835f5f101849d90197492e2d91b12a864cc
yunusyunu94 <125892629+yunusyunu94@users.noreply.github.com>
2024-10-29 11:07:10 +0300

    19_Varlık_Siniflari_Olusturma_Data_Klasoru_StudentCS

---
+++ CollegeApp/Data/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CollegeApp.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string SutudentName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
