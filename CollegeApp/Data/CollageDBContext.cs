bcff82d3952f4991846c43e95c9b258ed42e39a3
yunusyunu94 <125892629+yunusyunu94@users.noreply.github.com>
2024-11-03 09:50:38 +0300

    22_CollageDBContextdeki_Configurayyonlari_Ayri_Sinifa_Tasidik

--- CollegeApp/Data/CollageDBContext.cs
+++ CollegeApp/Data/CollageDBContext.cs
@@ -1,4 +1,5 @@
-﻿using CollegeApp.Models;
+﻿using CollegeApp.Data.Config;
+using CollegeApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CollegeApp.Data
@@ -18,34 +19,41 @@ namespace CollegeApp.Data
        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().HasData(new List<Student>()
-            {
-                new Student { 
-                    Id = 1, 
-                    SutudentName= "yunus",
-                    Address="Türkiye",
-                    Email="yunus@gmail.com",
-                    DOB= new DateTime(2024,12,12)
+            //modelBuilder.Entity<Student>().HasData(new List<Student>()
+            //{
+            //    new Student { 
+            //        Id = 1, 
+            //        SutudentName= "yunus",
+            //        Address="Türkiye",
+            //        Email="yunus@gmail.com",
+            //        DOB= new DateTime(2024,12,12)
 
-                },
-                new Student {
-                    Id = 2,
-                    SutudentName= "yusuf",
-                    Address="Türkiye",
-                    Email="yusuf@gmail.com",
-                    DOB= new DateTime(2024,6,12)
+            //    },
+            //    new Student {
+            //        Id = 2,
+            //        SutudentName= "yusuf",
+            //        Address="Türkiye",
+            //        Email="yusuf@gmail.com",
+            //        DOB= new DateTime(2024,6,12)
 
-                },
-            });  // Elle veri girisi yaptik;
+            //    },
+            //});  // Elle veri girisi yaptik;
+
+            // YUKARİDAKİ VERİ EKLEMEYİCE OLUSTURDUGUUZ DATA İCİNDE CONFİG DOSYASİNDAN EKLİYORUZ
+
+            //modelBuilder.Entity<Student>(entity =>
+            //{
+            //    entity.Property(n => n.SutudentName).IsRequired();
+            //    entity.Property(n => n.SutudentName).HasMaxLength(250);
+            //    entity.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            //    entity.Property(n => n.Email).IsRequired().HasMaxLength(250);
+            //});
+
+            // YUKARİDAKİ KONFİGURASYANU DATA İCİNDE CONFİG DOSYASİNA TASİTİK
 
 
-            modelBuilder.Entity<Student>(entity =>
-            {
-                entity.Property(n => n.SutudentName).IsRequired();
-                entity.Property(n => n.SutudentName).HasMaxLength(250);
-                entity.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
-                entity.Property(n => n.Email).IsRequired().HasMaxLength(250);
-            });
+            // Table1
+            modelBuilder.ApplyConfiguration(new StudentConfig());
 
         }
 
